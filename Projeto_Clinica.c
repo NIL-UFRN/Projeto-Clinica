d58e297bbffefff6afa3cad51fbe165f9e101e16
Francenildo <francenildo.costa.017@ufrn.edu.br>
2025-03-19 16:51:25 -0300

    Adiciona telas de cadastro e atualização ao sistema de agendamento

--- Projeto_Clinica.c
+++ Projeto_Clinica.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (void){
+    system("color 0a");
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
     printf("@@                                                         @@\n");
     printf("@@                 SISTEMA DE AGENDAMENTO                  @@\n");
@@ -18,7 +20,89 @@ int main (void){
     printf("@@           0 -> SAIR                                     @@\n");
     printf("@@                                                         @@\n");
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("Precione a tecla ENTER para continuar...");
     getchar();
 
+    system("cls || clear");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@                    TELA DE CADASTRO                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           1 -> CADASTRAR MEDICO                         @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           2 -> CADASTRAR PACIENTE                       @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           3 -> CADASTRAR CONSULTA                       @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           0 -> SAIR                                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("Precione a tecla ENTER para continuar...");
+    getchar();
+
+    system("cls || clear");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@                    TELA DE ATUALIZACAO                  @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           1 -> ATUALIZAR MEDICO                         @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           2 -> ATUALIZAR PACIENTE                       @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           3 -> ATUALIZAR CONSULTA                       @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           0 -> SAIR                                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("Precione a tecla ENTER para continuar...");
+    getchar();    
+
+
+    system("cls || clear");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@                    TELA DE PESQUISA                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           1 -> PESQUISAR MEDICO                         @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           2 -> PESQUISAR PACIENTE                       @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           3 -> PESQUISAR CONSULTA                       @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           0 -> SAIR                                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("Precione a tecla ENTER para continuar...");
+    getchar();    
+
+
+    system("cls || clear");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@                    TELA DE EXCLUSAO                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           1 -> DELETAR MEDICO                           @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           2 -> DELETAR PACIENTE                         @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           3 -> DELETAR CONSULTA                         @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           0 -> SAIR                                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("Precione a tecla ENTER para continuar...");
+    getchar();  
+    
+    
+    
     return 0;
+
 }
