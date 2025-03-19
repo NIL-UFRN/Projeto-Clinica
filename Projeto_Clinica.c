a7744b8d3004f0792d478727047b33e2e0abead7
Francenildo <francenildo.costa.017@ufrn.edu.br>
2025-03-19 15:00:57 -0300

    Criação da tela inicial

--- Projeto_Clinica.c
+++ Projeto_Clinica.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
 int main (void){
-    printf("Seja Bem Vindo!\n");
-    
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@                 SISTEMA DE AGENDAMENTO                  @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           1 -> CADASTRAR                                @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           2 -> ATUALIZAR                                @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           3 -> PESQUISAR                                @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           4 -> DELETAR                                  @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@           0 -> SAIR                                     @@\n");
+    printf("@@                                                         @@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    getchar();
+
     return 0;
 }
