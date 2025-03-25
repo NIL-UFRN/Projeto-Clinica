4b148ec6e84a50d576a788efbab01f86702ef90d
Francenildo <francenildo.costa.017@ufrn.edu.br>
2025-03-25 17:56:32 -0300

    Remove funções de cadastro e agendamento do módulo principal

--- Projeto_Clinica.c
+++ Projeto_Clinica.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h> //biblioteca para colorir o terminal
+#include "cadastrar.h"
 
 //prototipos das funcoes
 char menu_prim (void);
-void menu_cadastro (void);
-void cadastrar_medico (void);
-void cadastrar_paciente (void);
-void agendar_consulta (void);
 void menu_atualizacao (void);
 void menu_pesquisa (void);
 void menu_deletar (void);
 void informacoes (void);
 
 
-int main (void){
+int main (void) {
     char op ;
     op = menu_prim();
     switch (op)
@@ -79,91 +76,6 @@ char menu_prim (void) {
     scanf("%c", &op);
     getchar();
     return op;
-}
-
-void menu_cadastro (void) {    
-    system("cls || clear");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@                    TELA DE CADASTRO                     @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           1 -> CADASTRAR MEDICO                         @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           2 -> CADASTRAR PACIENTE                       @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           3 -> AGENDAR CONSULTA                         @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           0 -> VOLTAR                                   @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("Precione a tecla ENTER para continuar...");
-    getchar();
-} 
-
-void cadastrar_medico (void) {
-    system("cls || clear");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@                  CADASTRAR MEDICO                       @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           NOME:                                         @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           CPF:                                          @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           ESPECIALIZACAO:                               @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("Precione a tecla ENTER para continuar...");
-    getchar();
-}
-
-void cadastrar_paciente (void) {
-    system("cls || clear");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@                  CADASTRAR PACIENTE                     @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           NOME:                                         @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           IDADE:                                        @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           CPF:                                          @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@           SEXO:                                         @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("Precione a tecla ENTER para continuar...");
-    getchar();
-
-}
-
-void agendar_consulta (void) {
-    system("cls || clear");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@                  AGENDAR CONSULTA                       @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("@@                                                         @@\n");
-    printf("@@   DATA:                                                 @@\n");
-    printf("@@   PACIENTE:                                             @@\n");
-    printf("@@   CPF:                                IDADE:            @@\n");
-    printf("@@   SEXO:                     CONTATO:                    @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@   MEDICO:                                               @@\n");
-    printf("@@   ESPECIALIZACAO:                                       @@\n");
-    printf("@@                                                         @@\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-    printf("Precione a tecla ENTER para continuar...");
-    getchar();
-
 }
     
 
