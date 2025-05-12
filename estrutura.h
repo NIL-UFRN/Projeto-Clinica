b28738bc5996d68baa9db2a32fa3f099c8d6efac
Francenildo <francenildo.costa.017@ufrn.edu.br>
2025-05-12 17:45:17 -0300

    adiciona definições das estruturas Paciente, Medico e Consulta

---
+++ estrutura.h
@@ -0,0 +1,25 @@
+typedef struct paciente Paciente;
+
+struct paciente {
+    char nome[50];
+    char CPF[15];
+    char email[50];
+    char data_nascimento[12];
+    char sexo;
+    char contato[15];
+};
+
+typedef struct medico Medico;
+struct medico {
+    char nome[50];
+    char CPF[15];
+    char especialidade[50];
+    char contato[15];
+};
+
+typedef struct consulta Consulta;
+struct consulta {
+    char data[10];
+    Paciente paciente;
+    Medico medico;
+};
